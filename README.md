8208c654d524c9c4cd1b27403532611193fe032d
Philip van Oosten <user@localho.st>
2013-02-24 00:27:46 +0100

    Added initial version of the Python script to convert svg to html canvas.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 inkinetic
 =========
+by Philip van Oosten (@pvoosten)
 
 Convert an Inkscape drawing to an HTML5 canvas, using the Kinetic JS library.
+
+
---
+++ canvas.js
@@ -0,0 +1,144 @@
+var stage = new Kinetic.Stage({
+        container:'container',
+        width:744.09448819,
+        height:1052.3622047
+        });
+
+// Create a new layer
+var layer2 = new Kinetic.Layer();
+var g3076 = new Kinetic.Group();
+var path3082 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "m 199.96312,733.48579 41.59878,41.87851 0,-125.97538 -41.59878,-78.96697 z",
+        fill: 'green'
+        });
+g3076.add(path3082) // add to the g3076 group
+var path3078 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "m 199.96312,733.48579 0,-163.06384 -157.221076,-59.14271 0,193.33234 z",
+        fill: 'green'
+        });
+g3076.add(path3078) // add to the g3076 group
+var path3080 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "M 199.96312,733.48579 241.5619,775.3643 130.74447,759.72336 42.742044,704.61158 z",
+        fill: 'green'
+        });
+g3076.add(path3080) // add to the g3076 group
+var path3088 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "m 42.742044,704.61158 88.002426,55.11178 0,-143.30894 -88.002426,-105.13518 z",
+        fill: 'green'
+        });
+g3076.add(path3088) // add to the g3076 group
+var path3086 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "M 199.96312,570.42195 241.5619,649.38892 130.74447,616.41442 42.742044,511.27924 z",
+        fill: 'green'
+        });
+g3076.add(path3086) // add to the g3076 group
+var path3084 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "m 241.5619,775.3643 0,-125.97538 -110.81743,-32.9745 0,143.30894 z",
+        fill: 'green'
+        });
+g3076.add(path3084) // add to the g3076 group
+layer2.add(g3076) // add g3076 to layer
+var rect3090 = new Kinetic.Rect({
+        x: 79.9702912,
+        y: 269.998925,
+        width: 141.31448,
+        height: 126.48637,
+        fill: 'red'
+        });
+rect3090.setAbsolutePosition(0.0, 0.0);
+    rect3090.setRotation(-0.411926477458);
+    rect3090.setScale(1.00000000262, 1.0000000005);
+layer2.add(rect3090) // add rect3090 to layer
+var text3092 = new Kinetic.Text({
+        x:10,
+        y:10,
+        text:"Hello",
+        fill:'red',
+        fontSize:20
+        });
+layer2.add(text3092) // add text3092 to layer
+var path3096 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "M 228.57143,426.6479 149.6653,425.38987 103.89587,489.67765 80.708993,414.24471 5.4241407,390.58142 69.999995,345.21933 l -0.759168,-78.91251 63.096943,47.39763 74.81566,-25.10732 -25.57979,74.65544 z",
+        fill: 'green'
+        });
+layer2.add(path3096) // add path3096 to layer
+stage.add(layer2) // add the layer to the stage
+
+// Create a new layer
+var layer3 = new Kinetic.Layer();
+var g3040_7 = new Kinetic.Group();
+var path2985_1_4 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "M 528.86641,617.17706 C 524.4815,700.9028 544.83587,767.9327 470.36254,788.16652 395.88922,808.40034 271.68947,880.16743 235.31139,810.38029 207.10151,756.26289 330.12661,727.06296 353.81526,670.8194 384.79361,597.268 473.2513,613.4513 528.86641,617.17706 z",
+        fill: 'green'
+        });
+g3040_7.add(path2985_1_4) // add to the g3040_7 group
+var path3017_4_2 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "m 391.72355,642.89134 c 15.6151,23.72575 25.119,95.27472 -9.93243,73.84661 -43.50015,-26.59308 -105.15034,8.79666 -120.76544,-14.92909 -15.6151,-23.72575 10.80041,-50.63556 47.07529,-70.98945 45.32822,-25.43374 68.00748,-11.65382 83.62258,12.07193 z",
+        fill: 'green'
+        });
+g3040_7.add(path3017_4_2) // add to the g3040_7 group
+g3040_7.setAbsolutePosition(117.14285, -120.00001);
+layer3.add(g3040_7) // add g3040_7 to layer
+stage.add(layer3) // add the layer to the stage
+
+// Create a new layer
+var layer1 = new Kinetic.Layer();
+var g3044 = new Kinetic.Group();
+var path2985 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "m 502.85713,243.79076 c -49.6923,67.52705 -69.54062,134.70853 -142.87347,110.66738 -73.33285,-24.04115 -216.53433,-32.37343 -208.55509,-110.66738 6.18761,-60.71411 125.00595,-17.47044 175.71428,-51.42857 66.31282,-44.40807 131.30621,17.74112 175.71428,51.42857 z",
+        fill: 'green'
+        });
+path2985.setAbsolutePosition(-80.153541, 219.99945);
+    path2985.setRotation(-0.582083241154);
+    path2985.setScale(1.00000000043, 1.00000000043);
+g3044.add(path2985) // add to the g3044 group
+var path3017 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "m 454.67461,270.22658 c 0,28.40321 -31.39635,93.39435 -48.89505,56.22498 -21.71656,-46.12858 -92.67019,-50.45999 -92.67019,-78.8632 0,-28.40322 36.85946,-36.35917 78.35043,-33.41849 51.84612,3.67459 63.21481,27.65349 63.21481,56.05671 z",
+        fill: 'green'
+        });
+path3017.setAbsolutePosition(-80.153541, 219.99945);
+    path3017.setRotation(-0.582083241154);
+    path3017.setScale(1.00000000043, 1.00000000043);
+g3044.add(path3017) // add to the g3044 group
+g3044.setAbsolutePosition(111.42857, 0.0);
+layer1.add(g3044) // add g3044 to layer
+var g3040 = new Kinetic.Group();
+var path2985_1 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "M 528.86641,617.17706 C 524.4815,700.9028 544.83587,767.9327 470.36254,788.16652 395.88922,808.40034 271.68947,880.16743 235.31139,810.38029 207.10151,756.26289 330.12661,727.06296 353.81526,670.8194 384.79361,597.268 473.2513,613.4513 528.86641,617.17706 z",
+        fill: 'green'
+        });
+g3040.add(path2985_1) // add to the g3040 group
+var path3017_4 = new Kinetic.Path({
+        x: 0,
+        y: 0,
+        data: "m 391.72355,642.89134 c 15.6151,23.72575 25.119,95.27472 -9.93243,73.84661 -43.50015,-26.59308 -105.15034,8.79666 -120.76544,-14.92909 -15.6151,-23.72575 10.80041,-50.63556 47.07529,-70.98945 45.32822,-25.43374 68.00748,-11.65382 83.62258,12.07193 z",
+        fill: 'green'
+        });
+g3040.add(path3017_4) // add to the g3040 group
+g3040.setAbsolutePosition(28.571429, -120.0);
+layer1.add(g3040) // add g3040 to layer
+stage.add(layer1) // add the layer to the stage
