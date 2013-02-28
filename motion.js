4f3e01f5919b7178419e8c87e17dbb5afdb3cc28
Philip van Oosten <user@localho.st>
2013-02-28 22:45:43 +0100

    Added an example of how to use offset in Kinetic.js.

--- motion.js
+++ motion.js
@@ -5,4 +5,23 @@ moveWolk = function(){
 };
 moveWolk();
 
+// Make things move in this file.
+
+var r = new Kinetic.Rect({x:200 + 100/2, y:900 + 5/2, width: 100, height: 5, fill: '#FF0000',
+offset: {x:100/2, y:5/2},
+// rotationDeg: -30,
+});
+var rr = new Kinetic.Rect({x:200, y:900, width: 100, height: 5, fill: '#FF0000'});
+layer2.add(r);
+layer2.add(rr);
+layer2.draw();
+
+turn = function(){
+	r.setRotation(0);
+	r.transitionTo({rotation: Math.PI, duration: 5, callback: turn});
+}
+
+turn();
+
+
 
